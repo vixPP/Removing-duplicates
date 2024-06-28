3e0db7e1f7f201e4ca95efbcb6822c959eb5991d
vix <churink9@gmail.com>
2024-06-28 18:37:21 +0300

    first commit

---
+++ 2. Удаление дубликатов.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <set>
+
+int main() 
+{
+    int n;
+    std::cout << "[IN]:" << std::endl;
+    std::cin >> n;
+
+    std::set<int> numbers;
+    for (int i = 0; i < n; i++) 
+    {
+        int x;
+        std::cin >> x;
+        numbers.insert(x);
+    }
+
+    std::cout << "[OUT]: " << std::endl;
+
+    for (auto it = numbers.rbegin(); it != numbers.rend(); ++it) 
+    {
+        std::cout << *it << std::endl;
+    }
+
+    return 0;
+}
